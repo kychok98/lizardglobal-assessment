4f0563eb791154602cc1899e58aa582b43053550
kychok98 <kychok98@gmail.com>
2023-04-15 10:34:33 +0800

    style(app): update tailwind.config; index.css add directive apply;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#343d4c',
+        secondary: '#4d7397',
+        'light-secondary': '#d3dff7',
+        'light-primary': '#edf6ff',
+      },
+    },
   },
   plugins: [],
 };
