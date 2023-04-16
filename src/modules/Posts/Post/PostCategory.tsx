5b391169d4fdbe333456e4b8d81cf008bd1efd34
kychok98 <kychok98@gmail.com>
2023-04-16 16:53:50 +0800

    feat(post): PostCategory highlight selected category

--- src/modules/Posts/Post/PostCategory.tsx
+++ src/modules/Posts/Post/PostCategory.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { getUniqueBy } from 'src/utils';
 
 interface Props extends Pick<IPost, 'categories'> {}
 
 const PostCategory = ({ categories }: Props) => {
+  const [searchParams] = useSearchParams();
+
+  const selectedCategories = useMemo(() => {
+    return searchParams.getAll('category');
+  }, [searchParams]);
+
   return (
     <div className="flex flex-wrap gap-1 text-xs md:gap-2">
       {getUniqueBy(categories, 'name').map(category => {
+        const selected = selectedCategories.includes(category.name);
         return (
           <h5
             key={category.id}
-            className={`rounded border border-neutral-600 px-1.5 py-0.5 text-neutral-600`}
+            className={`rounded border border-neutral-600 px-1.5 py-0.5 text-neutral-600 ${
+              selected ? 'bg-secondary font-semibold text-light-primary' : ''
+            }`}
           >
             {category.name}
           </h5>
